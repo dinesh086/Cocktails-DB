40ab02031eef05e0ef977f5cdb3df08bd806dd5f
dinu-dinesh <dineshkumarmca086@gmail.com>
2020-08-14 18:06:13 +0530

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title"> about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero
+        necessitatibus delectus saepe illum dicta debitis eligendi mollitia,
+        illo magni, dolore quas ea pariatur voluptate natus veniam quam id nihil
+        consequatur?
+      </p>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/pages/SingleCocktail.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const SingleCocktail = () => {
+  const { id } = useParams();
+  return <h1>single cocktail page id : {id}</h1>;
+};
+
+export default SingleCocktail;
