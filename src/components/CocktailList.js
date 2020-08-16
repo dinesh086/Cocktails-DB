cbe37e5b8dd31e0f3048ea88b12330def8291a83
dinu-dinesh <dineshkumarmca086@gmail.com>
2020-08-16 11:52:56 +0530

    cocktaillist

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -1,5 +1,7 @@
 import React from "react";
+import Cocktail from "./Cocktail";
 
-export default function CocktailList() {
+export default function CocktailList(cocktails) {
+  console.log(cocktails);
   return <h1>cocktailList component</h1>;
 }
