eb1f1e3b4e5e7042d380d8952e34b969de918cea
dinu-dinesh <dineshkumarmca086@gmail.com>
2020-08-16 11:53:31 +0530

    cocktails

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -2,6 +2,12 @@ import React from "react";
 import Cocktail from "./Cocktail";
 
 export default function CocktailList(cocktails) {
+  if (loading) {
+    return <h2 className="section-title">loading....</h2>;
+  }
+  if (cocktails.lenth < 1) {
+    return h2.section - title;
+  }
   console.log(cocktails);
   return <h1>cocktailList component</h1>;
 }
