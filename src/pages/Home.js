e0ca4996b6d8ffb59ef923785688ff9d0e14a831
dinu-dinesh <dineshkumarmca086@gmail.com>
2020-08-16 11:55:23 +0530

    final home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,55 @@
 import React from "react";
+import CocktailsList from "../components/CocktailList";
+import SearchForm from "../components/SearchForm";
 
 export default function Home() {
-  return <h1>Home page</h1>;
+  const [loading, setLoading] = React.useState(false);
+  const [searchTerm, setSearchTerm] = React.useState("d");
+  const [cocktails, setCocktails] = React.useState([]);
+
+  React.useEffect(() => {
+    setLoading(true);
+    async function getDrinks() {
+      try {
+        const response = await fetch(
+          `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`
+        );
+        const data = await response.json();
+        const { drinks } = data;
+
+        if (drinks) {
+          const newCocktails = drinks.map((item) => {
+            console.log(item);
+            const {
+              idDrink,
+              strDrink,
+              strDrinkThumb,
+              strAlcholic,
+              strGlass,
+            } = item;
+            return {
+              id: idDrink,
+              name: strDrink,
+              image: strDrinkThumb,
+              info: strAlcholic,
+              glass: strGlass,
+            };
+          });
+          setCocktails(newCocktails);
+        } else {
+          setCocktails([]);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    }
+    getDrinks();
+  }, [searchTerm]);
+  return (
+    <main>
+      <SearchForm setSearchTerm={setSearchTerm} />
+      <CocktailsList loading={loading} cocktails={cocktails} />
+    </main>
+  );
 }
